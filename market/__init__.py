aefa9d2edc647e007f754bd192d5d66b5771bc83
DarkShadowKonoha <girishjha444@gmail.com>
2023-01-03 10:13:54 +0530

    Project finished

---
+++ market/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = 'ac313db8a7b2a17b6c99e911'
+db = SQLAlchemy(app)
+app.app_context().push()
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
+from market import routes
